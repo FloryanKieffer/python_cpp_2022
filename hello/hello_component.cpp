f25c29664253c258c17cb7f24d38962d657153e4
FloryanKieffer <kieffer.floryan@gmail.com>
2022-05-05 09:38:25 +0000

    Ajout de la fonction add dans hello_component.cpp

--- hello/hello_component.cpp
+++ hello/hello_component.cpp
@@ -10,13 +10,17 @@ char const* greet() {
   return "hello, world";
 }
 
+int const add(int a,int b){
+	return a+b;
+}
+
 namespace py = pybind11;
 
 
 PYBIND11_MODULE(hello_component,greetings)
 {
   greetings.doc() = "greeting_object 1.0";
-  
+  greetings.def("add",&add,"a function adding two integers"); 
   greetings.def("greet", &greet, "a function saying hello");
   greetings.def("getVersion", &getVersion, "a function returning the version");
 }
